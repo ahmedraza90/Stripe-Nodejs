90af680a70aa5a7812d2aad25fa44e566f7885cb
ahmed <34ahmed.raza@gmail.com>
2021-11-20 10:40:47 -0800

    jj

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ app.post('/create-checkout-session',async (req,res)=>{
 //route webhook
 app.post('/webhook',express.raw({type: 'application/json'}),(req,res)=>{
     console.log(req.body)
-    res.send(req.type)
+    res.send(req.body.type)
     // const sig = req.headers['stripe-signature']
     // try {
     //     events = stripe.webhooks.constructEvent(req.body, sig, 'whsec_dtAr4abS9Z5eyz1zrSQzUm76Pc0v8iyO');
