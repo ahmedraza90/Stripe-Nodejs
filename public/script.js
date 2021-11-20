fcecae8c86a0e405ee6f64170f574f7369b14c99
ahmed <34ahmed.raza@gmail.com>
2021-11-19 17:35:32 -0800

    route

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("button")
 button.addEventListener("click",async  () => {
 try{
-            const request = await fetch("/kk",{
+            const request = await fetch("/create-checkout-session",{
               method:"POST",
               headers:{
                    "Content-Type":"application/json"
