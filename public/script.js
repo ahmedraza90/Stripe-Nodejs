4184159138bf22f76ccbf38e0ed4f2f240fc2f27
ahmed <34ahmed.raza@gmail.com>
2021-11-20 03:09:45 -0800

    error

--- public/script.js
+++ public/script.js
@@ -1,21 +1,22 @@
 const button = document.querySelector("button")
 button.addEventListener("click",async  () => {
 try{
-            const request = await fetch("/create-checkout-session",{
-              method:"POST",
-              headers:{
-                   "Content-Type":"application/json"
-              },
-              body:JSON.stringify({
-                items:[
-                  {id:1,quantity:3},
-                  {id:2,quantity:4},
-                ],
-              }),
-            })
-            const {url} = await request.json()
-            window.location= url
-}catch(e){
-  console.log(e)
-}
+     const request = await fetch("/create-checkout-session"
+    ,{
+     method:"POST",
+     headers:{
+      "Content-Type":"application/json"
+     },
+     body:JSON.stringify({
+      items:[
+          {id:1,quantity:3},
+          {id:2,quantity:4},
+      ],
+    }),
+    }
+    )
+    console.log(request)
+      const {url} = await request.json()
+      window.location= url
+}catch(e){console.log(e)}
 })
--- src/server.js
+++ src/server.js
@@ -14,64 +14,59 @@ const store_products = new Map([
 ]) 
 //routes
 app.post('/create-checkout-session',async (req,res)=>{
-    res.send('hello')
-//     try{ 
-//         const str_pay = await stripe.checkout.sessions.
-//         create({
-//             payment_method_types:['card'],
-//             mode:'payment',
-//             line_items: req.body.items.map((item)=>{
-//                 const product = store_products.get(item.id)
-//                 return {
-//                     price_data:{
-//                         currency:'usd',
-//                         product_data:{
-//                             name:product.name
-//                         },
-//                         unit_amount:product.priceincents
+    try{ 
+        const str_pay = await stripe.checkout.sessions.
+        create({
+            payment_method_types:['card'],
+            mode:'payment',
+            line_items: req.body.items.map((item)=>{
+                const product = store_products.get(item.id)
+                return {
+                    price_data:{
+                        currency:'usd',
+                        product_data:{
+                            name:product.name
+                        },
+                        unit_amount:product.priceincents
                         
-//                     },
-//                     quantity:item.quantity
-//                 }
-//             }),//here coming data would be use
-//             success_url:`${process.env.CLIENT_URL}/success.html`,
-//             cancel_url:`${process.env.CLIENT_URL}/cancel.html`
-//         })
-//         res.json({url:str_pay.url})
+                    },
+                    quantity:item.quantity
+                }
+            }),//here coming data would be use
+            success_url:`${process.env.CLIENT_URL}/success.html`,
+            cancel_url:`${process.env.CLIENT_URL}/cancel.html`
+        })
+        res.json({url:str_pay.url})
         
-//     }catch(e){
-//     res.status(500).json({error: e.message})
-// }
+    }catch(e){
+        console.log(e)
+    res.status(500).json({error: e.message})
+}
 })
 
-//route webhook
-app.post('/webhook',express.raw({type: 'application/json'}),(req,res)=>{
-    console.log('lllll')
-    const sig = req.body
-    try {
-        events = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-      } catch (err) {
-        return response.status(400).send(`Webhook Error: ${err.message}`);
+// //route webhook
+// app.post('/webhook',express.raw({type: 'application/json'}),(req,res)=>{
+//     console.log('lllll')
+//     const sig = req.body
+//     try {
+//         events = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+//       } catch (err) {
+//         return response.status(400).send(`Webhook Error: ${err.message}`);
         
-      }
-    switch(events.type){
-        case 'checkout.session.completed':
-            const session = events.data.object;
-            console.log('PaymentIntent was successful!');
-            break;
-        case 'payment_method.attached':
-            const paymentMethod = events.data.object;
-            console.log('PaymentMethod was attached to a Customer!');
-            break;
-        default:
-            console.log(`Unhandled event type ${events.type}`);
-    }
-    response.json({recieved:true})
-
-})
-
-app.get('/kk',(req,res)=>{
-    res.send('kkkk')
-})
+//       }
+//     switch(events.type){
+//         case 'checkout.session.completed':
+//             const session = events.data.object;
+//             console.log('PaymentIntent was successful!');
+//             break;
+//         case 'payment_method.attached':
+//             const paymentMethod = events.data.object;
+//             console.log('PaymentMethod was attached to a Customer!');
+//             break;
+//         default:
+//             console.log(`Unhandled event type ${events.type}`);
+//     }
+//     response.json({recieved:true})
+// })
 
 app.listen(process.env.PORT || 3000)    
